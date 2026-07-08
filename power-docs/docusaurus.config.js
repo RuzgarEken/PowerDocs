db65ea96a94bcd5c0a8ed1b81726726bdfe77f30
Tarık Özdemir <tarikozdemir@192.168.1.18>
2026-07-08 19:38:28 +0300

    project and organization name fix

--- power-docs/docusaurus.config.js
+++ power-docs/docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ruzgareken', // Usually your GitHub org/user name.
+  projectName: 'powerdocs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
